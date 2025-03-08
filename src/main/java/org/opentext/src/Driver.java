d51d2beac84f8ca77343012f3d7545c120259379
Sameer Grover <smrgrover2@gmail.com>
2025-03-09 02:42:05 +0530

    Completed Opentext assignments

--- src/main/java/org/opentext/src/Driver.java
+++ src/main/java/org/opentext/src/Driver.java
@@ -2,37 +2,52 @@ package org.opentext.src;
 
 import org.opentext.src.impl.TaskExecuterImpl;
 
+import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Driver {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        Main.TaskExecutor executor = new TaskExecuterImpl(executorService);
+        // Assumption for READ/WRITE operation we need synchronization.
 
-        Main.TaskGroup taskGroupA = new Main.TaskGroup(UUID.fromString("A"));
-        Main.TaskGroup taskGroupB = new Main.TaskGroup(UUID.fromString("B"));
-//        Main.TaskGroup taskGroupC = new Main.TaskGroup(UUID.fromString("C"));
+        // Concurrency Level 5
+        // Implemented Custom executer service with additional methods to create tasks
+        TaskExecuterImpl executor = new TaskExecuterImpl(5);
 
-        Future<Boolean> result1 = executor.submitTask(new Main.Task<Boolean>(UUID.fromString("1"), taskGroupA, Main.TaskType.WRITE, () -> {
-            System.out.println("Executed Task 1");
-            return true;
-        }));
+        // Tasks
+        // Groups {A, B} Write for Concurrent Write but Sequential for a group
+        // Group C represent Sequential Read
 
-        Future<Boolean> result2 = executor.submitTask(new Main.Task<Boolean>(UUID.fromString("2"), taskGroupA, Main.TaskType.WRITE, () -> {
-            System.out.println("Executed Task 1");
-            return true;
-        }));
+        // Group A, B represent Write
+        Main.TaskGroup taskGroupA = new Main.TaskGroup(UUID.randomUUID());
+        Main.TaskGroup taskGroupB = new Main.TaskGroup(UUID.randomUUID());
 
-        Future<Boolean> result3 = executor.submitTask(new Main.Task<Boolean>(UUID.fromString("3"), taskGroupB, Main.TaskType.WRITE, () -> {
-            System.out.println("Executed Task 1");
-            return true;
-        }));
+        // Group C represent Read
+        Main.TaskGroup taskGroupC = new Main.TaskGroup(UUID.randomUUID());
 
+        // Creating Write Tasks with Group A & Group B
+        List<Main.Task<String>> writeTasksGroupA = executor.createWriteTasks(2, taskGroupA);
+        List<Main.Task<String>> writeTasksGroupB = executor.createWriteTasks(2, taskGroupB);
 
+        // Creating Read Tasks with Group C
+        List<Main.Task<String>> readTasksGroupC = executor.createReadTasks(4, taskGroupC);
 
+        writeTasksGroupA.addAll(writeTasksGroupB);
+        writeTasksGroupA.addAll(readTasksGroupC);
+
+        // Submit all READ & WRITE tasks
+        List<Future<String>> futureList = writeTasksGroupA.stream().map(executor::submitTask).toList();
+
+        futureList.forEach(f -> {
+            try {
+                System.out.println("Task " + f.get() + " is completed");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // stopping executer service
+        executor.shutDown();
     }
 }
--- src/main/java/org/opentext/src/impl/TaskExecuterImpl.java
+++ src/main/java/org/opentext/src/impl/TaskExecuterImpl.java
@@ -2,19 +2,102 @@ package org.opentext.src.impl;
 
 import org.opentext.src.Main;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TaskExecuterImpl implements Main.TaskExecutor {
 
-    private ExecutorService executorService;
+    private final ExecutorService executorService;
+    ConcurrentHashMap<String, ReentrantReadWriteLock> groupLocks = new ConcurrentHashMap<>();
+    LinkedBlockingQueue<Main.Task<?>> queue = new LinkedBlockingQueue<>();
 
-    public TaskExecuterImpl(ExecutorService executorService){
-        this.executorService = executorService;
+    public TaskExecuterImpl(int concurrency){
+        executorService = Executors.newFixedThreadPool(concurrency);
     }
 
     @Override
     public <T> Future<T> submitTask(Main.Task<T> task) {
+        queue.offer(task);
         return executorService.submit(task.taskAction());
+    }
+
+    public void shutDown(){
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdownNow();
+        }
+    }
+
+    public List<Main.Task<String>> createWriteTasks(int capacity, Main.TaskGroup taskGroup){
+        List<Main.Task<String>> tasks = new ArrayList<>();
+        for(int i = 1; i <= capacity; i++){
+            tasks.add(createWriteTask(UUID.randomUUID(), taskGroup));
+        }
+        return tasks;
+    }
+
+    private Main.Task<String> createReadTask(UUID taskId, Main.TaskGroup taskGroup){
+        Main.Task<String> task = new Main.Task<>(taskId, taskGroup, Main.TaskType.READ, () -> {
+            ReentrantReadWriteLock lock = groupLocks.computeIfAbsent(taskGroup.groupUUID().toString(), k -> new ReentrantReadWriteLock());
+            ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+            ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+            Main.Task<?> queuedTask = queue.poll();
+            try {
+                if(queuedTask.taskType().equals(Main.TaskType.READ)){
+                    readLock.lock();
+                    System.out.println("Task " + taskId.toString() + " with group " + taskGroup.groupUUID().toString() + " consuming " + queuedTask.taskUUID().toString() + " with consumed group " + queuedTask.taskGroup().groupUUID().toString() + " performing read operation. ");
+                    Thread.sleep(500);
+                    return taskId.toString();
+                } else {
+                    writeLock.lock();
+                    System.out.println("Task " + taskId.toString() + " with group " + taskGroup.groupUUID().toString() + " consuming " + queuedTask.taskUUID().toString() + " with consumed group " + queuedTask.taskGroup().groupUUID().toString() + " performing read operation. ");
+                    Thread.sleep(500);
+                    return taskId.toString();
+                }
+            } catch (InterruptedException e) {
+                return taskId.toString();
+            } finally {
+                if(queuedTask.taskType().equals(Main.TaskType.READ)){
+                    readLock.unlock();
+                } else {
+                    writeLock.unlock();
+                }
+            }
+        });
+        return task;
+    }
+
+    public Main.Task<String> createWriteTask(UUID taskId, Main.TaskGroup taskGroup){
+        Main.Task<String> task = new Main.Task<>(taskId, taskGroup, Main.TaskType.WRITE, () -> {
+            ReentrantReadWriteLock lock = groupLocks.computeIfAbsent(taskGroup.groupUUID().toString(), k -> new ReentrantReadWriteLock());
+            ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+            try {
+                writeLock.lock();
+                System.out.println("Task " + taskId + " with group " + taskGroup.groupUUID().toString() + " performing write operation. ");
+                Thread.sleep(500);
+                return taskId.toString();
+            } catch (InterruptedException e) {
+                return taskId.toString();
+            } finally {
+                writeLock.unlock();
+            }
+        });
+        queue.offer(task);
+        return task;
+    }
+
+    public List<Main.Task<String>> createReadTasks(int capacity, Main.TaskGroup taskGroup) {
+        List<Main.Task<String>> tasks = new ArrayList<>();
+        for(int i = 1; i <= capacity; i++){
+            tasks.add(createReadTask(UUID.randomUUID(), taskGroup));
+        }
+        return tasks;
     }
 }
