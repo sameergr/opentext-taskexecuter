936f28d240e41f773dc0621e60348b2173d0ff46
Sameer Grover <smrgrover2@gmail.com>
2025-03-09 02:48:23 +0530

    finialize assignment

--- src/main/java/org/opentext/src/Driver.java
+++ src/main/java/org/opentext/src/Driver.java
@@ -8,6 +8,15 @@ import java.util.concurrent.*;
 
 public class Driver {
 
+    // Requirements
+    //  1. Tasks can be submitted concurrently. Task submission should not block the submitter.
+    //  2. Tasks are executed asynchronously and concurrently. Maximum allowed concurrency may be restricted.
+    //  3. Once task is finished, its results can be retrieved from the Future received during task submission.
+    //  4. The order of tasks must be preserved.
+        //  a. The first task submitted must be the first task started.
+        //  b. The task result should be available as soon as possible after the task completes.
+    //5. Tasks sharing the same TaskGroup must not run concurrently.
+
     public static void main(String[] args) {
         // Assumption for READ/WRITE operation we need synchronization.
 
